f40a240e9fe12a34001cc90507983fa947fc5420
Connor Murphy <murphyc97@gmail.com>
2020-02-18 21:52:41 -0500

    init the changes

--- src/App.js
+++ src/App.js
@@ -1,15 +1,32 @@
 import React, { Component } from "react";
 import "./App.css";
 import { Navbar } from "react-bootstrap";
+import { Nav } from "react-bootstrap";
+import { NavItem } from "react-bootstrap";
 import { Link } from "react-router-dom";
+import Routes from "./Routes";
 
 export default class App extends Component {
   render() {
     return (
       <div className="App container">
         <Navbar bg="light" expand="lg">
-          <Link to="/">Fisher Bookstore</Link>
+          <Navbar.Brand>
+            <Link to="/">Fisher Bookstore</Link>
+          </Navbar.Brand>
+          <Navbar.Toggle aria-controls="basic-navbar-nav" />
+          <Navbar.Collapse className="justify-content-end">
+            <Nav>
+              <NavItem>
+                <Nav.Link href="/books">Books</Nav.Link>
+              </NavItem>
+              <NavItem>
+                <Nav.Link href="/authors">Authors</Nav.Link>
+              </NavItem>
+            </Nav>
+          </Navbar.Collapse>
         </Navbar>
+        <Routes />
       </div>
     );
   }
