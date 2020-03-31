16ccf8fd60673d2a6ec9b0245268cbbe5cd391ea
Connor Murphy <murphyc97@gmail.com>
2020-03-30 23:08:33 -0400

    init the changes

--- src/authors/authors.js
+++ src/authors/authors.js
@@ -1,38 +1,20 @@
-import React, { Component } from "react";
-import "./authors.css";
+import React, { useEffect, useState } from "react";
 import { AuthorDisplay } from "./AuthorDisplay";
 
-export default class Authors extends Component {
-    constructor(props) {
-        super(props);
-        this.state = {
-            authors: [
-                {
-                    id: 1,
-                    aName: "Eric Evans",
-                    birthDate: "12/12/65"
-                },
-                {
-                    id: 2,
-                    aName: "Nicole Forsgren",
-                    birthDate: "1/2/70"
-                },
-                {
-                    id: 3,
-                    aName: "George RR Martin",
-                    birthDate: "8/20/48"
-                }
-            ]
-        };
-    }
+export default function Authors(props) {
+    const [data, setData] = useState([]);
 
-    render() {
-        return(
-            <div className="Authors">
-                <div className="lander">
-                    <AuthorDisplay authors={this.state.authors} />
-                </div>
+    useEffect(() => {
+        fetch("http://localhost:5001/api/authors/")
+        .then(response => response.json())
+        .then(data => setData(data));
+    }, []);
+
+    return (
+        <div className="Authors">
+            <div className="lander">
+                <AuthorDisplay authors={data} />
             </div>
-        );
-    }
-};
+        </div>
+    );
+}
--- src/books/books.js
+++ src/books/books.js
@@ -1,71 +1,20 @@
-import React, { Component } from "react";
-import "./books.css";
+import React, { useEffect, useState } from "react";
 import { BookDisplay } from "./BookDisplay";
 
-export default class Books extends Component {
-    constructor(props) {
-        super(props);
-        this.state = {
-            books: [
-                {
-                    id: 1,
-                    title: "Domain Driven Design",
-                    author: "Eric Evans",
-                    isbn: "978-0321125217"
-                },
-                {
-                    id: 2,
-                    title: "Accelerate",
-                    author: "Nicole Forsgren",
-                    isbn: "978-1942788331"
-                },
-                {
-                    id: 3,
-                    title: "A Game of Thrones",
-                    author: "George RR Martin",
-                    isbn: "978-0553386790"
-                },
-                {
-                    id: 4,
-                    title: "A Clash of Kings",
-                    author: "George RR Martin",
-                    isbn: "978-9173488297"
-                },
-                {
-                    id: 5,
-                    title: "A Storm of Swords",
-                    author: "George RR Martin",
-                    isbn: "978-8293059998"
-                },
-                {
-                    id: 6,
-                    title: "A Feast for Crows",
-                    author: "George RR Martin",
-                    isbn: "978-9024567157"
-                },
-                {
-                    id: 7,
-                    title: "A Dance with Dragons",
-                    author: "George RR Martin",
-                    isbn: "978-0553390568"
-                },
-                {
-                    id: 8,
-                    title: "The Winds of Winter",
-                    author: "George RR Martin",
-                    isbn: "978-0008165130"
-                }
-            ]
-        };
-    }
+export default function Books(props) {
+    const [data, setData] = useState([]);
 
-    render() {
-        return (
-            <div className="Books">
-                <div className="lander">
-                    <BookDisplay books={this.state.books} />
-                </div>
+    useEffect(() => {
+        fetch("http://localhost:5001/api/books/")
+        .then(response => response.json())
+        .then(data => setData(data));
+    }, []);
+
+    return (
+        <div className="Books">
+            <div className="lander">
+                <BookDisplay books={data} />
             </div>
-        );
-    }
+        </div>
+    );
 }
